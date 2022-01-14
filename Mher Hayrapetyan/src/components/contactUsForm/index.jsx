00b646cfbed01943e03acd33eea9f68b1a40ffdc
mherhay93 <mherhay93@gmail.com>
2022-01-14 19:36:24 +0400

    Home page, vers. 1

---
+++ Mher Hayrapetyan/src/components/contactUsForm/index.jsx
@@ -0,0 +1,19 @@
+function ContactUsForm() {
+  return (
+    <form>
+      <p>CONTACT US</p>
+      <div>
+        <p>Keep in Touch</p>
+        <p>With Harmoni</p>
+      </div>
+      <input/>
+      <input/>
+      <input/>
+      <input/>
+      <textarea></textarea>
+    </form>
+  );
+}
+
+
+export default ContactUsForm;
