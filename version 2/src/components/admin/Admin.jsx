576084e9aba9ac9b5bee1e5c4fd095376f478fe4
Hasmik20 <61650766+Hasmik20@users.noreply.github.com>
2022-01-19 18:28:35 +0400

    add events test

--- version 2/src/components/admin/Admin.jsx
+++ version 2/src/components/admin/Admin.jsx
@@ -1,30 +1,117 @@
-import "./admin.css";
-import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react'
+import './admin.css'
+import { Link } from 'react-router-dom'
+import { useGlobalContext } from '../../context/EventContext'
 
 const Admin = () => {
+    const [newEvent, setNewEvent] = useState({
+    title: "",
+    picture: "",
+    date: "",
+    registered: "",
+    field: "",
+    ticket:"",
+      info: "",
+    address: "",
+  })
+  
+  const { setEvents, events,addEvents } = useGlobalContext()
+ 
+  const changeInputs = (e) => {
+    const { value, name } = e.target
+    setNewEvent ({
+      ...newEvent,
+      isActive:false,
+        [name]: value}) 
+  }
+  const handelSubmit = (e) => {
+    e.preventDefault()
+    // addEvents(newEvent)
+    fetch('http://localhost:8000/events', {
+      method: "POST",
+     body: JSON.stringify(newEvent),
+      headers: { 'Content-type': 'application/json; charset=UTF-8' }
+    }).then(res=>{
+      if(!res.ok){
+     throw Error("could not fetch tha data")
+    }
+    return res.json();
+    }).then(data =>{
+    setEvents([data, ...events])
+      setNewEvent({
+       title: "",
+    picture: "",
+    date: "",
+    registered: "",
+    field: "",
+    ticket:"",
+      info: "",
+    address: "",
+    })
+  }).catch(err => {
+    console.log(err.message)
+  })
+  }
+
   return (
     <div>
-      <form className="form-admin" action="" method="">
+      <form className="form-admin"
+        onSubmit={handelSubmit}>
         <div className="form-admin-title">
           <p>Add new event</p>
         </div>
         <div className="admin-container-inputs">
-          <input className="eventTitle" placeholder="Event title" />
-          <input className="eventImage" placeholder="Image" />
-          <input className="eventDate" placeholder="Date: eg. 21 Feb" />
-          <input className="eventDate" placeholder="Time: eg. 16:00 - 19:30" />
-          <select>
+          <input className="eventTitle" placeholder="Event title"
+            value = {newEvent.title}
+            name="title" 
+            
+            onChange = {changeInputs}
+          />
+          <input className="eventImage" placeholder="Image"
+            value = {newEvent.picture}
+            name="picture" 
+            onChange = {changeInputs}
+          />
+          <input className="eventDate" placeholder="Date: eg. 21 Feb"
+            value = {newEvent.date}
+            name="date"
+            onChange = {changeInputs}
+          />
+          <input className="eventDate" placeholder="Time: eg. 16:00 - 19:30"
+           value = {newEvent.registered}
+            name="registered"
+            onChange = {changeInputs}
+          />
+            <input className="eventDate" placeholder="Price: eg. ֏4500"
+            value = {newEvent.ticket}
+            name="ticket" 
+            onChange = {changeInputs}
+          />
+            <input className="eventDate" placeholder="Address"
+            value = {newEvent.address}
+            name="address" 
+            onChange = {changeInputs}
+          />
+          <select value = {newEvent.field}
+            name = "field"
+            onChange = {changeInputs}>
             <option value="music">Music</option>
             <option value="education">Education</option>
             <option value="culture">Culture</option>
             <option value="movie">Movie</option>
             <option value="tourism">Tourism</option>
           </select>
-          <Link to="/events">
+           <textarea className="yousMessage" placeholder="Description"
+            value = {newEvent.info}
+            name = "info"
+             onChange = {changeInputs}
+          ></textarea>
+           <button className="formButton" type="submit">ADD Event</button>
+          {/* <Link to="/events">
             <button className="formButtonAdmin" type="submit">
               ADD Event
             </button>
-          </Link>
+          </Link> */}
         </div>
       </form>
     </div>
