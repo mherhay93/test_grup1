23334ad29cfde44899df334791619338e23865e9
mherhay93 <mherhay93@gmail.com>
2022-01-19 18:37:40 +0400

    Register func

--- version 2/src/components/login/iogin.jsx
+++ version 2/src/components/login/iogin.jsx
@@ -1,8 +1,9 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import "../login/login.css";
 
-import { IoCloseOutline } from "react-icons/io5";
-
 const Login = () => {
+
   return (
     <div className="login-container">
       <div className="login-box">
@@ -10,8 +11,8 @@ const Login = () => {
           <p>Account Login</p>
         </div>
         <form className="login-form">
-          <input type="text" placeholder="User Nik" />
-          <input type="text" placeholder="Password" />
+          <input type="text" placeholder="User Nik"/>
+          <input type="password" placeholder="Password" />
           <button type="submit">LOGIN</button>
         </form>
       </div>
--- version 2/src/components/register/register.jsx
+++ version 2/src/components/register/register.jsx
@@ -1,6 +1,59 @@
+import { useState, useEffect } from "react";
+
 import "../register/register.css";
 
 const Register = () => {
+  const [nameUser, setNameUser] = useState("");
+  const [surnameUser, setSurnameUser] = useState("");
+  const [userNik, setUserNik] = useState("");
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [submit, setSubmit] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSubmit(!submit);
+  };
+
+  const controler = () => {};
+
+  useEffect(
+    (data) => {
+      data = {
+        userName: nameUser,
+        userSurname: surnameUser,
+        userNik: userNik,
+        email: emailAddress,
+        password: password,
+      };
+
+      fetch(" http://localhost:8000/users ")
+        .then((response) => {
+          return response.json();
+        })
+        .then((res) => {
+          for (let i in data) {
+            res.map((item) => {
+              console.log(Object.values(item).includes(nameUser));
+              if (nameUser !== "") {
+                if (!Object.values(item).includes(nameUser)) {
+                  fetch("http://localhost:8000/users", {
+                    method: "POST",
+                    body: JSON.stringify(data),
+                    headers: {
+                      "Content-type": "application/json;charset=utf-8",
+                    },
+                  });
+                  break
+                }
+              }
+            });
+          }
+        });
+    },
+    [submit]
+  );
+
   return (
     <div className="register-container">
       <div className="register-box">
@@ -12,17 +65,55 @@ const Register = () => {
           </div>
         </div>
         <form className="register-form">
-          <input type="text" placeholder="User Name" />
-          <input type="text" placeholder="User Surname" />
-          <input type="text" placeholder="User Nik" />
-          <input type="text" placeholder="Email Address" />
-          <input type="text" placeholder="Password" />
+          <input
+            type="text"
+            placeholder="User Name"
+            onChange={(e) => {
+              setNameUser(e.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="User Surname"
+            onChange={(e) => {
+              setSurnameUser(e.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="User Nik"
+            onChange={(e) => {
+              setUserNik(e.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Email Address"
+            onChange={(e) => {
+              setEmailAddress(e.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
           <input type="text" placeholder="Repeat Password" />
           <div className="checkBoxInput">
-            <label><input type="checkbox" className="adiminBox" name="admin" />Admin</label>
-            <label><input type="checkbox" className="userBox" name="admin" />User</label>
+            <label>
+              <input type="checkbox" className="adiminBox" name="admin" />
+              Admin
+            </label>
+            <label>
+              <input type="checkbox" className="userBox" name="admin" />
+              User
+            </label>
           </div>
-          <button type="submit">LOGIN NOW</button>
+          <button type="submit" onClick={handleSubmit}>
+            LOGIN NOW
+          </button>
         </form>
       </div>
     </div>
